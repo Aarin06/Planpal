92d07d3f95a2dc5e9412c8a738a4b6902325d759
Wenduo <jacky.ji12345@gmail.com>
2024-06-24 17:45:43 -0400

    Restructured file layout

---
+++ backend/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import cred from "./PlanPalCred.json" assert { type: "json" };
+
+const serviceAccount = cred;
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
+export default db;
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBdj6gMDTgiD2Fybki9EUwbXYKi1oKFtek",
+//   authDomain: "planpal-427121.firebaseapp.com",
+//   projectId: "planpal-427121",
+//   storageBucket: "planpal-427121.appspot.com",
+//   messagingSenderId: "407535876944",
+//   appId: "1:407535876944:web:d1d79b31aaff63642c0a55"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
