7e64d6ae38ee39bb01803974e389605c01d9c569
Wenduo <jacky.ji12345@gmail.com>
2024-06-25 10:02:11 -0400

    Added an itinerary creating page

---
+++ frontend/src/app/pages/itinerary-setup/itinerary-setup.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItinerarySetupComponent } from './itinerary-setup.component';
+
+describe('ItinerarySetupComponent', () => {
+  let component: ItinerarySetupComponent;
+  let fixture: ComponentFixture<ItinerarySetupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ItinerarySetupComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ItinerarySetupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
