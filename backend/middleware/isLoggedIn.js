bb71808f5544b106bb0091e63f4a77a8c94ce920
Aarin06 <aarinjasikumar@gmail.com>
2024-06-28 00:59:02 -0400

    fix some styling

--- backend/middleware/isLoggedIn.js
+++ backend/middleware/isLoggedIn.js
@@ -1,4 +1,5 @@
 export function isLoggedIn(req, res, next) {
   console.log("in middle ware");
+  console.log("aarin",req.user);
   req.user ? next() : res.status(401).json({ error: "Something went wrong" });
 }
--- backend/routers/protected.js
+++ backend/routers/protected.js
@@ -4,6 +4,7 @@ export const protectedRouter = Router();
 
 protectedRouter.get("/", (req, res, next) => {
   console.log("okokok");
+  console.log("jacky",req.user);
   // res.send(`Hello ${req.user.displayName}`);
   res.redirect("http://localhost:4200/");
   // next()
