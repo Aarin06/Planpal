846411463ab5bf9fd913f2659a6bd528eef0ba04
Wenduo <jacky.ji12345@gmail.com>
2024-07-13 11:44:17 -0400

    Added event patching and ability to store searched results

--- backend/routers/events_router.js
+++ backend/routers/events_router.js
@@ -5,6 +5,50 @@ import { ItineraryMember } from "../models/itineraryMembers.js";
 
 export const eventsRouter = Router();
 
+eventsRouter.get("/:id", async (req, res) => {
+  const eventId = req.params.id
+  if (!eventId){
+    return res.status(422).json({ error: "eventId is required." });
+  }
+  try {
+    const event = await Event.findByPk(eventId);
+    if (!event){
+      return res.status(404).json({error: "Event not found"})
+    }
+    return res.json(event)
+  } catch (error) {
+    return res.status(500).json({error: error})
+  }
+})
+
+eventsRouter.patch("/:id", async (req, res) => {
+  const eventId = req.params.id
+  if (!eventId){
+    return res.status(422).json({ error: "eventId is required." });
+  }
+  
+  try {
+    const event = await Event.findByPk(eventId);
+    if (!event) {
+      return res.status(404).json({ error: "Event Cannot Be Found" });
+    }
+
+    // Assuming `req.body` contains the fields to update
+    const updatedEvent = await event.update({
+      title: req.body.title,
+      location: req.body.extendedProps.location,
+      start: req.body.start,
+      end: req.body.end,
+      allDay: req.body.allDay
+    });
+
+    return res.json(updatedEvent);
+  } catch (error) {
+    // Handle potential errors, such as validation errors or database errors
+    return res.status(500).json({ error: "Cannot Update The Given Event" });
+  }
+})
+
 eventsRouter.delete("/:id", async (req, res) => {
   const eventId = req.params.id
   if (!eventId){
