3b82b84b25036256fb368fd39d2807287aa73ebd
Aarin06 <aarinjasikumar@gmail.com>
2024-07-26 17:00:08 -0400

    revert change

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://api.planpal.tech/auth/google/callback",
+      callbackURL: "http://localhost:3000/auth/google/callback",
       passReqToCallback: true,
     },
     async function (request, accessToken, refreshToken, profile, done) {
